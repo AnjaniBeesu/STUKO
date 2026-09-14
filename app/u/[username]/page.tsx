6c0082bdd91f7a77f02e7dbbcf72708b92c6b5cd
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-14 12:48:28 +0530

    Remove Firebase path alias from public profile

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
-import { firebaseConfigured, getFirebase } from '@/lib/firebase';
+import { firebaseConfigured, getFirebase } from '../../../lib/firebase';
 import { MessageCircle, UserPlus } from 'lucide-react';
 import styles from './public.module.css';
 
